0f035b6b8493d114e93685c5307c6eef208273ed
cjamison <elocjam13@gmail.com>
2026-01-12 11:09:51 -0500

    Initialized Spring

---
+++ src/main/java/com/zipcode/invested/InvestEdApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.invested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestEdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zipcode/invested/InvestEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.invested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
